027aec5946472bb5d2fd1200814b254fbc5e228e
Su Khai <khai_373@hotmail.com>
2015-04-19 01:49:44 -0700

    Documented DatabaseManager.java.

--- app/src/main/java/com/example/sukhai/part_it/Database/DatabaseManager.java
+++ app/src/main/java/com/example/sukhai/part_it/Database/DatabaseManager.java
@@ -1,13 +1,26 @@
 package com.example.sukhai.part_it.Database;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import java.io.File;
-
 /**
+ * A class that handle opening and closing of the database connection.
+ *
+ * This is a singleton class, so DatabaseManager.initializeInstance(Context) must be called first
+ * before any interaction with the methods in this class.
+ *
+ * In order to connect and disconnect from the database from other classes, you must call
+ * DatabaseManager.getInstance().open() to open the database connection and
+ * DatabaseManager.getInstance().close() to close the database connection.
+ *
+ * Example of usage:
+ *
+ *      SQLiteDatabase db = DatabaseManager.getInstance().open();
+ *          ...
+ *          ...
+ *      DatabaseManager.getInstance().close();
+ *
  * Created by Su Khai Koh on 4/17/15.
  */
 public class DatabaseManager {
@@ -16,12 +29,16 @@ public class DatabaseManager {
     private static DatabaseHelper mDatabaseHelper;
     private SQLiteDatabase mDatabase;
 
-    private int mOpenCounter;
+    private int mOpenCounter;               // Number of connections opened
 
     private DatabaseManager() {
-
+        // Default constructor
     }
 
+    /**
+     * Initialize this class.
+     * @param context The context of the application
+     */
     public static synchronized void initializeInstance(Context context) {
 
         if (instance == null) {
@@ -32,6 +49,10 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Get the instance of this class.
+     * @return the instance of this class
+     */
     public static synchronized DatabaseManager getInstance() {
 
         if (instance == null) {
@@ -42,6 +63,11 @@ public class DatabaseManager {
         return instance;
     }
 
+    /**
+     * Open a connection to the database. This method will make sure there is only one connection
+     * open through out the application life time.
+     * @return the database that is connected to
+     */
     public synchronized SQLiteDatabase open() {
 
         mOpenCounter++;
@@ -53,6 +79,9 @@ public class DatabaseManager {
         return mDatabase;
     }
 
+    /**
+     * Close the connection to the database.
+     */
     public synchronized void close() {
 
         mOpenCounter--;
@@ -63,6 +92,11 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Check whether the given table in the database is empty (no rows) or not.
+     * @param tableName the table to be checked in the database
+     * @return true if the table is empty (no rows), otherwise false.
+     */
     public boolean isEmpty(String tableName) {
 
         boolean empty = true;
