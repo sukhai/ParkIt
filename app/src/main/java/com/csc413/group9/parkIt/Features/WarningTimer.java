30fe20d9a470d57036816d644b2e19b540d15b65
Sai Krishna Undurthi <sai.undurthi@gmail.com>
2015-04-25 23:52:28 -0700

    Implemented class Warning Timer

--- app/src/main/java/com/csc413/group9/parkIt/Features/WarningTimer.java
+++ app/src/main/java/com/csc413/group9/parkIt/Features/WarningTimer.java
@@ -1,5 +1,12 @@
 
 package com.csc413.group9.parkIt.Features;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimerTask;
 
@@ -61,81 +68,67 @@ import android.widget.TextView;
 
 class WarningTimer extends TimerTask {
 
-        @Override
+    Timer timer;
+    TimerTask timerTask;
 
-        public void run() {
+    //we are going to use a handler to be able to run in our TimerTask
+    final Handler handler = new Handler();
 
-            System.out.println("Start time:" + new Date());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-            doSomeWork();
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-            System.out.println("End time:" + new Date());
+        //onResume we start our timer so it can start when the app comes from the background
+        startTimer();
+    }
 
-        }
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
 
+        //initialize the TimerTask's job
+        initializeTimerTask();
 
+        //schedule the timer, after the first 5000ms the TimerTask will run every 10000ms
+        timer.schedule(timerTask, 5000, 10000); //
+    }
 
-                // simulate a time consuming task
-
-        private void doSomeWork() {
-
-            try {
-
-
-                Thread.sleep(10000);
-
-            } catch (InterruptedException e) {
-
-                e.printStackTrace();
-
-            }
-
-        }
-
-
-
-        public static void main(String args[]) {
-
-
-
-            TimerTask timerTask = new WarningTimer();
-
-            // running timer task as daemon thread
-
-            Timer timer = new Timer(true);
-
-            timer.scheduleAtFixedRate(timerTask, 0, 10 * 1000);
-
-            System.out.println("TimerTask begins! :" + new Date());
-
-            // cancel after sometime
-
-            try {
-
-                Thread.sleep(20000);
-
-            } catch (InterruptedException e) {
-
-                e.printStackTrace();
-
-            }
-
+    public void stoptimertask(View v) {
+        //stop the timer, if it's not already null
+        if (timer != null) {
             timer.cancel();
-
-            System.out.println("TimerTask cancelled! :" + new Date());
-
-            try {
-
-                               Thread.sleep(30000);
-
-            } catch (InterruptedException e) {
-
-                e.printStackTrace();
-
-            }
-
+            timer = null;
         }
+    }
 
+    public void initializeTimerTask() {
+
+        timerTask = new TimerTask() {
+            public void run() {
+
+                //use a handler to run a toast that shows the current timestamp
+                handler.post(new Runnable() {
+                    public void run() {
+                        //get the current timeStamp
+                        Calendar calendar = Calendar.getInstance();
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd:MMMM:yyyy HH:mm:ss a");
+                        final String strDate = simpleDateFormat.format(calendar.getTime());
+
+                        //show the toast
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(getApplicationContext(), strDate, duration);
+                        toast.show();
+                    }
+                });
+            }
+        };
+    }
 
 
     }
