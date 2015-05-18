8d0dab0c72975c425ace1ada6efb934a9d6bf81c
Su Khai <khai_373@hotmail.com>
2015-05-18 00:33:11 -0700

    Documented SplashActivity and remove unnecessary methods.

--- app/src/main/java/com/csc413/group9/parkIt/SplashActivity.java
+++ app/src/main/java/com/csc413/group9/parkIt/SplashActivity.java
@@ -2,33 +2,43 @@ package com.csc413.group9.parkIt;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
 import com.csc413.group9.parkIt.SFPark.ParkingInformation;
 import com.google.android.gms.maps.GoogleMap;
 
-
+/**
+ * Create a splash screen when the app just started. When this splash screen is shown, the app
+ * will also fetch SFPark data at the back, so when the SFPark data will be ready to draw on the
+ * map when this splash screen is gone.
+ */
 public class SplashActivity extends Activity {
 
-    final int SPLASH_LENGTH = 5000;
-    ParkingInformation mParkingInfo;
-    GoogleMap mMap;
+    /**
+     * The time to show this splash screen.
+     */
+    private final int SPLASH_LENGTH = 5000;
 
+    /**
+     * A reference to the ParkingInformation.
+     */
+    private ParkingInformation mParkingInfo;
 
+    /**
+     * A reference to the Google Map.
+     */
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //
+        // Load SFPark data
         loadData();
 
-        //Timer for the Splash screen
+        // Timer for the Splash screen
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -37,34 +47,14 @@ public class SplashActivity extends Activity {
                 SplashActivity.this.finish();
             }
         }, SPLASH_LENGTH);
-
     }
-    //Calling the webservice to get the parking data
+
+    /**
+     * Fetch and load the SFPark data from the SFPark website.
+     */
     private void loadData() {
         mParkingInfo = new ParkingInformation(this, mMap);
 
         mParkingInfo.getSFParkData();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
